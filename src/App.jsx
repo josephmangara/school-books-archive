8bb0d2f744c7e5201ff6ff1c5ad3f96ee18b3f82
josephmangara <josephmangara2@gmail.com>
2025-10-23 23:59:38 +0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
-import Footer from './components/Footer'
-import Header from './components/Header'
+import Footer from './components/Footer';
+import Header from './components/Header';
 import './App.css'
 
 function App() {
@@ -8,6 +8,14 @@ function App() {
   return (
     <>
       <Header />
+      <main className="flex-grow max-w-6xl mx-auto px-4 py-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/primary" element={<PrimaryBooks />} />
+          <Route path="/highschool" element={<HighSchoolBooks />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
       <Footer />
     </>
   )
