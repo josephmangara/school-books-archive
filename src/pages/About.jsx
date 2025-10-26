fde36a83fd26d15bed439d7d4a523afda16d923b
josephmangara <josephmangara2@gmail.com>
2025-10-26 23:02:24 +0300

    about and home page intro

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,9 @@ export default function About(){
 
   return (
     <>
-      <h1>About</h1>
+      <h1>About the project</h1>
+      <p>School Books Archive is a documentation of the literature I encountered throughout school.
+        It preserves memories of the books that everyone who studied under the 8-4-4 system read.</p>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <>
       <h1>Book repository</h1>
+      <p>This project documents the primary school storybooks we read in the 2000s and 2010s. It also includes the highschool setbooks, both English and Kiswahili books, that have been examined in KCSE since the inception of the 8-4-4 system.</p>
     </>
   )
 }
