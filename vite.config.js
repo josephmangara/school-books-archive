4d995434f8d19a288d8ea0ddf98f0b8c72c907ca
josephmangara <josephmangara2@gmail.com>
2025-12-02 23:33:37 +0300

    server connection

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:5555",
         changeOrigin: true,
-        secure: false
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       }
     }
   }
