f9b071e565d695cbad6b1b816d7a7adcadd2a36b
josephmangara <josephmangara2@gmail.com>
2026-03-03 03:03:31 +0300

    added vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      manifest: {
+        name: "School books",
+        short_name: "Books",
+        icons: [
+          {
+            src: '/favicon-512.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/favicon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#e5d3b8",
+        background_color: "#ffebcd"
+      }
+    })
   ],
 
   server: {
